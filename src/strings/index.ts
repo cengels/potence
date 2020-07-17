492a5ce0451efe96f0888fb577278cbb1778699d
cengels <cengels@tuta.io>
2020-07-17 21:55:15 +0200

    Add string tests, fix Strings.strip()

--- src/strings/index.ts
+++ src/strings/index.ts
@@ -5,8 +5,19 @@
 export function strip(from: string, ...what: string[]): string {
     let string = from;
 
+    if (what.length === 0) {
+        what.push(' ');
+    }
+
     for (const substring of what) {
-        string = string.replace(substring, '');
+        // Previously this solution used string.replace(), but since you also had to
+        // check string.includes() on every iteration, it was only about half as fast.
+        let index = string.indexOf(substring);
+
+        while (index !== -1) {
+            string = string.slice(0, index) + string.slice(index + substring.length);
+            index = string.indexOf(substring);
+        }
     }
 
     return string;
--- tests/Strings.test.ts
+++ tests/Strings.test.ts
@@ -1,5 +1,7 @@
-import { Nullable } from '../src';
+import * as Strings from '../src/strings';
 
-const test: Nullable<number> = 1;
-
-Nullable.exists('1')
+describe('Strings.strip() should', () => {
+    it('strip all spaces by default', () => expect(Strings.strip('     k     t      ')).toBe('kt'));
+    it('strip all specified occurrences', () => expect(Strings.strip('banana apple bananana', 'banana')).toBe(' apple na'));
+    it('strip all strings', () => expect(Strings.strip('foo bar foobar foo and bar', 'foo', 'bar', ' ')).toBe('and'));
+});
