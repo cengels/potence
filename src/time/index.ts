8da4487af8d019322fd90dd2656d162e69da263a
cengels <cengels@tuta.io>
2022-03-18 10:22:03 +0100

    Remove redundant Time functions

--- src/time/index.ts
+++ src/time/index.ts
@@ -1,19 +1,21 @@
-/**
- * Gets the time (in milliseconds) that passed since
- * the specified `Date` or millisecond timestamp.
- */
-export function passed(since: Date | number): number {
-    if (typeof since === 'number') {
-        return Date.now() - since;
-    }
-    
-    return Date.now() - since.getTime();
-}
+import CompareResult from '../numbers/CompareResult.js';
 
 /** 
- * Returns true if at least the given time (in ms) has passed
- * since the specified `Date` or millisecond timestamp.
+ * Compares two Dates or their contained millisecond values with each other
+ * and returns a result indicating if the first argument is greater, smaller,
+ * or equal to the second argument.
  */
-export function hasPassed(since: Date | number, ms: number): boolean {
-    return passed(since) >= ms;
+export function compare(a: Date | number, b: Date | number): CompareResult {
+    const aTime = typeof a === 'number' ? a : a.getTime();
+    const bTime = typeof b === 'number' ? b : b.getTime();
+
+    if (aTime < bTime) {
+        return CompareResult.Less;
+    }
+
+    if (aTime > bTime) {
+        return CompareResult.Greater;
+    }
+
+    return CompareResult.Equal;
 }
