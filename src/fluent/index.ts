e4c3ebb1b7f75f9b1f2414c56ce212d194291578
cengels <cengels@tuta.io>
2022-03-19 13:05:01 +0100

    Add map<T, U>()

--- src/fluent/index.ts
+++ src/fluent/index.ts
@@ -16,3 +16,21 @@ export function is<T>(...objects: T[]): FluentIs<T> {
 
     return fluentIs as FluentIs<T>;
 }
+
+/**
+ * Maps one or multiple objects to a result.
+ * 
+ * This function is useful to compress multiple lines into one, especially
+ * for results based on multiple or repeated values, e.g.:
+ * 
+ * @example
+ * // get penultimate element of array
+ * // would otherwise require you to store myArray
+ * // in a local variable
+ * map(getMyArray(), array => array[array.length - 2]);
+ */
+export function map<T extends unknown[], U>(...args: [...objects: T, callback: (...objects: T) => U]): U {
+    const callback = args[args.length - 1] as (...objects: T) => U;
+
+    return callback(...args.slice(0, -1) as T);
+}
