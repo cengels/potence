4b44a1b4883826b7dfb3dcef3176251df11b974e
cengels <cengels@tuta.io>
2022-09-18 12:58:28 +0200

    Rework Queue using a circular buffer
    This implementation is still untested.

--- src/data/Queue.ts
+++ src/data/Queue.ts
@@ -1,13 +1,30 @@
+import { previousIndex } from '../arrays';
+
+const Empty = Symbol('empty-queue-element');
+const QUEUE_BUFFER_SIZE = 24 as const;
+
 /**
- * An implementation of a queue in JavaScript using a linked list.
- * A queue is first-in-first-out (FIFO), so you can only ever add elements
- * to the back of the queue but remove elements from the front of the queue.
+ * Represents a queue, i.e. a collection of elements structured in a
+ * first-in-first-out (FIFO) manner. New elements are added to the *back*
+ * of the queue, but elements are always retrieved from the *front*
+ * of the queue.
  * 
- * Note that, since linked lists generally aren't placed in contiguous memory,
- * performance is often subpar compared to an array-based queue.
+ * ## Internals and performance
+ * 
+ * This type is implemented using a
+ * [circular buffer](https://en.wikipedia.org/wiki/Circular_buffer).
+ * This makes the performance on most operations O(1). The only exception
+ * is when a new element is to be added, but the internal buffer is already
+ * at capacity, in which case the buffer must first be resized.
+ * Resizing does carry some performance penalty with it but is generally fast
+ * enough not to be a concern for most code. If it is, however, you may use
+ * {@link Queue.withCapacity}() to construct a Queue with a specified base
+ * capacity, eliminating the need for resizing.
  */
 export default class Queue<T> {
-    private readonly array: T[];
+    private readonly array: (T | typeof Empty)[];
+    private start: number = 0;
+    private end: number;
 
     /** 
      * Constructs a new Queue.
@@ -15,14 +32,80 @@ export default class Queue<T> {
      * @param elements Initial elements to add to the Queue.
      */
     public constructor(...elements: T[]) {
+        if (elements.length === 1 && elements[0] === Empty) {
+            // Small hack to make it possible for internals to construct
+            // a Queue without expanding it immediately.
+            this.array = [];
+            this.end = 0;
+            
+            return;
+        }
+
         this.array = elements;
+        this.end = elements.length - 1;
+
+        this.#expand();
+    }
+
+    /** 
+     * Constructs a new Queue with the given capacity. Useful for queues with a
+     * small initial size that may accept a great number of new elements.
+     */
+    public static withCapacity<T>(capacity: number): Queue<T> {
+        const queue = new Queue<T>(Empty as T);
+
+        queue.#expand(capacity);
+
+        return queue;
+    }
+
+    /** 
+     * Expands the internal array by either `QUEUE_BUFFER_SIZE` or the array's
+     * own length (doubling its size), setting the new elements to the
+     * {@link Empty} symbol.
+     */
+    #expand(to?: number): void {
+        const previousLength = this.array.length;
+        const expandBy = to != null
+            ? this.array.length - to
+            : this.array.length < QUEUE_BUFFER_SIZE
+                ? QUEUE_BUFFER_SIZE
+                : this.array.length;
+
+        for (let i: number = 0; i < expandBy; i++) {
+            this.array.push(Empty);
+        }
+
+        if (this.start > this.end) {
+            // Move all values at the end of the array up to the new end.
+            const lengthDifference = previousLength - this.start;
+
+            for (let i: number = this.start; i < previousLength; i++) {
+                this.array[i + lengthDifference] = this.array[i];
+                this.array[i] = Empty;
+            }
+
+            this.start += lengthDifference;
+        }
     }
 
     /**
-     * Adds an element to the back of the Queue.
+     * Adds an element to the back of the Queue. This is an O(1) operation,
+     * unless the queue is at capacity, in which case it has to be resized
+     * first.
      */
-    public enqueue(element: T): void {
-        this.array.unshift(element);
+    public enqueue(element: T): this {
+        const previousEnd = previousIndex(this.array, this.end);
+
+        if (this.start === previousEnd) {
+            this.#expand();
+        }
+
+        this.start = previousIndex(this.array, this.start);
+
+        this.array[this.start] = element;
+
+        return this;
     }
 
     /**
@@ -31,13 +114,17 @@ export default class Queue<T> {
      * This function will throw an error if the Queue is empty.
      */
     public dequeue(): T {
-        const head = this.array.pop();
-
-        if (head == null) {
-            throw new Error(`Can't dequeue(): Stack had no elements.`);
+        if (this.isEmpty()) {
+            throw new Error('Can\'t dequeue(): Queue is empty.');
         }
 
-        return head;
+        const previousEnd = previousIndex(this.array, this.end);
+
+        const head = this.array[this.end];
+        this.array[this.end] = Empty;
+        this.end = previousEnd;
+
+        return head as T;
     }
 
     /**
@@ -45,11 +132,15 @@ export default class Queue<T> {
      * If the Queue is empty, returns `undefined`.
      */
     public peek(): T | undefined {
-        return this.array[this.array.length - 1];
+        if (this.start === this.end) {
+            return undefined;
+        }
+
+        return this.array[this.end] as T;
     }
 
     /** Returns true if the Queue is empty. */
     public isEmpty(): boolean {
-        return this.array.length === 0;
+        return this.start === this.end;
     }
 }
