c15307cdccb6458bcb22882d37521fe4e058b3ee
cengels <cengels@tuta.io>
2024-02-08 09:19:31 +0100

    Update build options (compile to ES, not CommonJS)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export * as Arrays from './arrays/index.js';
 export * as Numbers from './numbers/index.js';
 export { default as Range } from './numbers/Range.js';
-export { default as ReadonlyRange } from './numbers/ReadonlyRange.js';
+export { type default as ReadonlyRange } from './numbers/ReadonlyRange.js';
 export { default as Flags } from './flags/index.js';
 export { default as List } from './arrays/List.js';
-export { default as ReadonlyList } from './arrays/ReadonlyList.js';
+export { type default as ReadonlyList } from './arrays/ReadonlyList.js';
 export { default as Version } from './numbers/Version.js';
 export { default as CompareResult } from './numbers/CompareResult.js';
 export { default as Queue } from './data/Queue.js';
