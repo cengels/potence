eadc51308e37b9a1ea48bbe3704b2c03c728fc76
cengels <cengels@tuta.io>
2024-02-08 11:43:14 +0100

    Don't re-export undocumented types

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ export { default as List } from './arrays/List.js';
 export { type default as ReadonlyList } from './arrays/ReadonlyList.js';
 export { default as Version } from './numbers/Version.js';
 export { default as CompareResult } from './numbers/CompareResult.js';
-export { default as Queue } from './data/Queue.js';
-export { default as Stack } from './data/Stack.js';
 export * as Objects from './objects/index.js';
 export * as Strings from './strings/index.js';
 export * as Assert from './assert/index.js';
